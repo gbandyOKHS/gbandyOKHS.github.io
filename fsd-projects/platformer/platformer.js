d62c7bed2c81b06ab1634f99a27f47f525fef376
gbandyOKHS <gabrielrbandy@gmail.com>
2026-02-12 15:20:16 +0000

    started game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,21 +27,42 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+    toggleGrid();
 
 
     // TODO 2 - Create Platforms
-
-
+createPlatform(0, 500, 350, 15);
+createPlatform(350, 150, 10, 500);
+createPlatform(300, 270, 60, 15);
+createPlatform(0, 375, 60, 5);
+createPlatform(350, 150, 700, 85);
+createPlatform(1380, 275, 20, 5);
+createPlatform(1049, 150, 5, 335);
+createPlatform(1225, 400, 175, 5);
+createPlatform(1050, 275, 10, 5);
+createPlatform(1050, 700, 350, 50);
+createPlatform(874, 350, 5, 600);
+createPlatform(1046, 500, 5, 3);
+createPlatform(800, 625, 100, 10);
+createPlatform(800, 380, 85, 10);
+createPlatform(350, 500, 350, 50);
+createPlatform(1390, 175, 5, 5);
 
 
     // TODO 3 - Create Collectables
-
+createCollectable("steve", 50, 275);
+createCollectable("diamond", 1375, 25);
+createCollectable("max", 375, 275);
+createCollectable("database", 50, 700);
+createCollectable("grace", 1025, 260);
 
 
     
     // TODO 4 - Create Cannons
-
+createCannon("left", 550, 100);
+createCannon("bottom", 1350, 25);
+createCannon("bottom", );
+createCannon("right", );
 
     
     
