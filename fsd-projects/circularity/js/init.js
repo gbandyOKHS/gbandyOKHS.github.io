9027c66dbbc18fffb66764039e04330b69624f89
gbandyOKHS <gabrielrbandy@gmail.com>
2026-04-27 13:18:04 +0000

    100

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -45,7 +45,7 @@ var init = function (window) {
         // TODO 3 : Call the drawCircle() function
        
         // TODO 7 : Use a loop to create multiple circles
-        for (var i = 0; i < 50; i++) {
+        for (var i = 0; i < 100; i++) {
             drawCircle();
         }
 
