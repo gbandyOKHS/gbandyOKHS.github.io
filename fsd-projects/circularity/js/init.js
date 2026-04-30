99b3a45fb4ad0a19d8f90468e9e45021ccf15cb9
gbandyOKHS <gabrielrbandy@gmail.com>
2026-04-30 14:23:25 +0000

    fixxed

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -25,7 +25,10 @@ var init = function (window) {
 
         // TODO 2 : Create a function that draws a circle 
         function drawCircle() {
-            circle = draw.randomCircleInArea(canvas, true, true, "#999", 2); physikz.addRandomVelocity(circle, canvas, 5, 5); view.addChild(circle); circles.push(circle); 
+            circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
+            physikz.addRandomVelocity(circle, canvas, 5, 5);
+            view.addChild(circle);
+            circles.push(circle); 
         }
         Gamification.init({
             canvas: canvas,
@@ -79,7 +82,7 @@ var init = function (window) {
         game.checkCirclePosition = function(circle) {
 
             // if the circle has gone past the RIGHT side of the screen then place it on the LEFT
-            if ( circle.x > canvas.width ) {
+            if (circle.x > canvas.width) {
                 circle.x = 0;
             } else if (circle.x < 0) {
                 circle.x = canvas.width;
