f3ca8d9aa83d058edff662fce7205cc8e65e322c
gbandyOKHS <gabrielrbandy@gmail.com>
2026-09-11 13:51:56 +0000

    improvements were made

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -32,21 +32,25 @@ $(function () {
 
     // TODO 2 - Create Platforms
 createPlatform(0, 500, 350, 15);
-createPlatform(350, 150, 10, 500);
+createPlatform(170, 498, 15, 15);
+createBadPlatform(0, 499, 350, 16);
+createPlatform(350, 165, 10, 485);
 createPlatform(280, 250, 80, 15);
 createPlatform(0, 375, 80, 5);
-createPlatform(350, 150, 700, 85);
+createPlatform(350, 165, 700, 70);
 createPlatform(1380, 275, 20, 5);
-createPlatform(1049, 150, 5, 335);
+createPlatform(1049, 165, 5, 335);
 createPlatform(1225, 400, 175, 5);
 createPlatform(1050, 275, 20, 5);
-createPlatform(1050, 700, 350, 50);
 createPlatform(874, 350, 5, 600);
 createPlatform(1026, 500, 20, 3);
 createPlatform(800, 625, 100, 10);
 createPlatform(800, 380, 85, 10);
-createPlatform(350, 500, 350, 50);
+createPlatform(450, 500, 250, 10);
+createFakePlatform(350, 500, 350, 10);
 createPlatform(175, 650, 50, 100);
+createPlatform(350, 650, 100, 5);
+createPlatform(450, 505, 5, 150);
 
     // TODO 3 - Create Collectables
 createCollectable("coin", 50, 275);
@@ -58,13 +62,14 @@ createCollectable("coin", 1025, 260);
 
     
     // TODO 4 - Create Cannons
-createCannon("left", 500, 2000);
-createCannon("bottom", 1250, 750);
-createCannon("bottom", 818, 2000);
-createCannon("right", 500, 5000);
-createCannon("bottom", 0, 1000);
-
-    
+createCannon("left", 475, 9999);
+createCannon("bottom", 1250, 9999);
+createCannon("bottom", 818, 9999);
+createCannon("right", 500, 9999);
+createCannon("bottom", 0, 9999);
+createCannon("left", -50, 0);
+createCannon("right", 68, 0);
+createCannon("bottom", 345, 2000000000);
     
     //////////////////////////////////
     // ONLY CHANGE ABOVE THIS POINT //
