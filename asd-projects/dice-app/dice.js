6c8778c5c6b86715eb0ede824df1c211f86f0b35
gbandyOKHS <gabrielrbandy@gmail.com>
2026-09-14 13:36:56 +0000

    added improvements to dice app

--- asd-projects/dice-app/dice.js
+++ asd-projects/dice-app/dice.js
@@ -1,23 +1,23 @@
 $(document).ready(function () {
   // Your code goes here
   $("<div>")
-  .css("height", 15)
-  .css("width", 15)
-  .css("background-color", "black")
-  .css("position", "absolute")
-  .css("top", 50)
-  .css("left", 50)
-  .appendTo("div");
-
-  function makeDot(top, left, elementID) {
-    $("<div>")
     .css("height", 15)
     .css("width", 15)
     .css("background-color", "black")
     .css("position", "absolute")
-    .css("top", top)
-    .css("left", left)
-    .appendTo(elementID);
+    .css("top", 50)
+    .css("left", 50)
+    .appendTo("<div>");
+
+  function makeDot(top, left, elementID) {
+    $("<div>")
+      .css("height", 15)
+      .css("width", 15)
+      .css("background-color", "black")
+      .css("position", "absolute")
+      .css("top", top)
+      .css("left", left)
+      .appendTo(elementID);
   }
   
   function rollDie(dieID) {
